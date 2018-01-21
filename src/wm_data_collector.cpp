46d778f471f1f850e41d7744934d4dbbdb31db14
Lucas Maurice <lucas.maurice@outlook.com>
2018-01-21 18:18:53 -0500

    Add Licence

---
+++ src/wm_data_collector.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lucas on 21/01/18.
+//
+
+#include "wm_data_collector_node.h"
+
+
+int main(int argc, char **argv) {
+
+
+
+
+	
+	return 0;
+}
---
+++ src/wm_data_collector.h
@@ -0,0 +1,13 @@
+//
+// Created by lucas on 21/01/18.
+//
+
+#ifndef PROJECT_WM_DATA_COLLECTOR_NODE_H
+#define PROJECT_WM_DATA_COLLECTOR_NODE_H
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+#endif //PROJECT_WM_DATA_COLLECTOR_NODE_H
