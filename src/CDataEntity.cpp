490da22f149f06ab55d8b38f7d131c33e7f87338
Lucas Maurice <lucas.maurice@outlook.com>
2018-01-21 23:51:36 -0500

    Add a method for set color Add a method for print the entity Add the entity name to the constructor Add a few getters and setters

--- src/CDataEntity.cpp
+++ src/CDataEntity.cpp
@@ -10,37 +10,53 @@
  * @param xmax	abscissa of the last point of the bounding box
  * @param ymax	ordinate of the last point of the bounding box
  */
-CDataEntity::CDataEntity(const int xmin, const int ymin, const int xmax, const int ymax){
-	BoundingBoxInitialised = false;
+CDataEntity::CDataEntity(const int xmin, const int ymin, const int xmax, const int ymax, const std::string name) {
+	this->BoundingBoxInitialised = false;
+	this->name = name;
+	this->color = "unknown";
 	setBoundingBox(xmin, ymin, xmax, ymax);
 }
 
- /**
-  * Initialise the bounding box of the entity
+/**
+ * Initialise the bounding box of the entity
  * @param xmin	abscissa of the first point of the bounding box
  * @param ymin	ordinate of the first point of the bounding box
  * @param xmax	abscissa of the last point of the bounding box
  * @param ymax	ordinate of the last point of the bounding box
-  * @return 	if the bounding box was correctly initialised
-  */
+ * @return 	if the bounding box was correctly initialised
+ */
 bool CDataEntity::setBoundingBox(const int xmin, const int ymin, const int xmax, const int ymax) {
 	// Test if the bounding box is a correct bounding box.
 	if ((xmax - xmin) > 0 && (ymax - ymin) > 0 && xmin > 0 && ymin > 0 && xmax > 0 && ymax > 0){
-		BoundingBoxInitialised = true;
-		this->Box.xmin = xmin;
-		this->Box.ymin = ymin;
-		this->Box.xmax = xmax;
-		this->Box.ymax = ymax;
+		this->BoundingBoxInitialised = true;
+		this->box.xmin = xmin;
+		this->box.ymin = ymin;
+		this->box.xmax = xmax;
+		this->box.ymax = ymax;
 	} else {
-		BoundingBoxInitialised = false;
-		this->Box.xmin = 0;
-		this->Box.ymin = 0;
-		this->Box.xmax = 0;
-		this->Box.ymax = 0;
+		this->BoundingBoxInitialised = false;
+		this->box.xmin = 0;
+		this->box.ymin = 0;
+		this->box.xmax = 0;
+		this->box.ymax = 0;
 	}
-	return BoundingBoxInitialised;
+	return this->BoundingBoxInitialised;
 }
 
+
+void CDataEntity::printEntity() {
+	std::cout << name << "(" << color << ")" << " : " << box.xmin << "x" << box.ymin << " to " << box.xmax << "x" << box.ymax << " " << std::endl;
+}
+
+/** --- --- --- --- GETTERS --- --- --- --- **/
 bool CDataEntity::isBoundingBoxInitialised() const {
 	return BoundingBoxInitialised;
+}
+const wm_color_detector::BoundingBox &CDataEntity::getBox() const {
+	return box;
+}
+
+/** --- --- --- --- SETTERS --- --- --- --- **/
+void CDataEntity::setColor(const std::string &color) {
+	CDataEntity::color = color;
 }
--- src/CDataEntity.h
+++ src/CDataEntity.h
@@ -5,17 +5,29 @@
 #ifndef PROJECT_CDATAENTITY_H
 #define PROJECT_CDATAENTITY_H
 
-#include "wm_data_collector/BoundingBox.h"
+#include "ros/ros.h"
+#include <sstream>
+#include "wm_color_detector/BoundingBox.h"
 
 class CDataEntity {
 public:
-	CDataEntity(int xmin, int ymin, int xmax, int ymax);
+	CDataEntity(const int xmin, const int ymin, const int xmax, const int ymax, const std::string name);
 	bool setBoundingBox(int xmin, int ymin, int xmax, int ymax);
+
+	void printEntity();
+
+	void setColor(const std::string &color);
+
 	bool isBoundingBoxInitialised() const;
+	const wm_color_detector::BoundingBox &getBox() const;
 
 private:
-	wm_data_collector::BoundingBox Box;
+	std::string name;
+	std::string color;
+	wm_color_detector::BoundingBox box;
 	bool BoundingBoxInitialised;
+
+
 
 };
 
