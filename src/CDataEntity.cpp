ee205046415f41326735f05a566a1f44ee09a065
Lucas Maurice <lucas.maurice@outlook.com>
2018-01-21 22:07:54 -0500

    Create the class of an entity

--- src/CDataEntity.cpp
+++ src/CDataEntity.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "CDataEntity.h"
+/**
+ * Initialise a DataEntity object with a bounding box
+ * @param xmin	abscissa of the first point of the bounding box
+ * @param ymin	ordinate of the first point of the bounding box
+ * @param xmax	abscissa of the last point of the bounding box
+ * @param ymax	ordinate of the last point of the bounding box
+ */
+CDataEntity::CDataEntity(const int xmin, const int ymin, const int xmax, const int ymax){
+	BoundingBoxInitialised = false;
+	setBoundingBox(xmin, ymin, xmax, ymax);
+}
+
+ /**
+  * Initialise the bounding box of the entity
+ * @param xmin	abscissa of the first point of the bounding box
+ * @param ymin	ordinate of the first point of the bounding box
+ * @param xmax	abscissa of the last point of the bounding box
+ * @param ymax	ordinate of the last point of the bounding box
+  * @return 	if the bounding box was correctly initialised
+  */
+bool CDataEntity::setBoundingBox(const int xmin, const int ymin, const int xmax, const int ymax) {
+	// Test if the bounding box is a correct bounding box.
+	if ((xmax - xmin) > 0 && (ymax - ymin) > 0 && xmin > 0 && ymin > 0 && xmax > 0 && ymax > 0){
+		BoundingBoxInitialised = true;
+		this->Box.xmin = xmin;
+		this->Box.ymin = ymin;
+		this->Box.xmax = xmax;
+		this->Box.ymax = ymax;
+	} else {
+		BoundingBoxInitialised = false;
+		this->Box.xmin = 0;
+		this->Box.ymin = 0;
+		this->Box.xmax = 0;
+		this->Box.ymax = 0;
+	}
+	return BoundingBoxInitialised;
+}
+
+bool CDataEntity::isBoundingBoxInitialised() const {
+	return BoundingBoxInitialised;
+}
--- src/CDataEntity.h
+++ src/CDataEntity.h
@@ -5,8 +5,17 @@
 #ifndef PROJECT_CDATAENTITY_H
 #define PROJECT_CDATAENTITY_H
 
+#include "wm_data_collector/BoundingBox.h"
 
 class CDataEntity {
+public:
+	CDataEntity(int xmin, int ymin, int xmax, int ymax);
+	bool setBoundingBox(int xmin, int ymin, int xmax, int ymax);
+	bool isBoundingBoxInitialised() const;
+
+private:
+	wm_data_collector::BoundingBox Box;
+	bool BoundingBoxInitialised;
 
 };
 
